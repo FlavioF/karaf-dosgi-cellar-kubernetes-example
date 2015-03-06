7b85f761b1ea467f2898078ad19a50070747518a
Flávio Ferreira <fferreira@bikeemotion.com>
2015-03-06 17:09:32 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# karaf-cellar-kubernetses-example
---
+++ dosgi-greeter/service/src/main/java/org/apache/karaf/cellar/samples/dosgi/greeter/service/GreeterImpl.java
@@ -0,0 +1,50 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.cellar.samples.dosgi.greeter.service;
+
+import java.util.Map;
+import org.apache.karaf.cellar.samples.dosgi.greeter.api.Greet;
+import org.apache.karaf.cellar.samples.dosgi.greeter.api.GreetResponse;
+import org.apache.karaf.cellar.samples.dosgi.greeter.api.Greeter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Implementation of the Greeter servicce.
+ */
+public class GreeterImpl implements Greeter {
+
+  private static final Logger log = LoggerFactory.getLogger(GreeterImpl.class);
+
+  private int counter = 0;
+  private String id;
+
+  public GreeterImpl(String id) {
+    this.id = id;
+  }
+
+  public void activate(Map<String, ?> configuration) {
+    log.info("Activating Greeter service.");
+  }
+
+  @Override
+  public GreetResponse greet(Greet greet) {
+    String message = greet.getMessage();
+    String response = message + "."
+        + String.format("Hello from node %s count %s.", id, counter++);
+    GreetResponse greetResponse = new GreetResponse(greet, response);
+    return greetResponse;
+  }
+
+}
