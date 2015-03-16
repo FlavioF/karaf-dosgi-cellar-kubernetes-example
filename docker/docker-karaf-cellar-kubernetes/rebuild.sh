535e40eba29f2e34a47c586e92b70eb254ed3871
Flávio Ferreira <fferreira@bikeemotion.com>
2015-03-16 20:05:43 +0000

    Updated property of hazelcast to do first merge 10 seconds after start Fixed some scripts and docker images

--- docker/docker-karaf-cellar-kubernetes/rebuild.sh
+++ docker/docker-karaf-cellar-kubernetes/rebuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --rm -t=fferreira/karaf-cellar-kubernetes .
+docker build --rm -t=flaviof/karaf-cellar-kubernetes .
--- docker/docker-karaf/rebuild.sh
+++ docker/docker-karaf/rebuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --rm -t=fferreira/karaf .
+docker build --rm -t=flaviof/karaf .
